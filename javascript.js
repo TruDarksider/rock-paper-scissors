ea6995016eb3e96dad2f42934a351db52e7072e2
Tyler Hopkin <tyler.hopkin12@gmail.com>
2022-08-08 16:20:08 -0500

    Update javasctript.js

--- javascript.js
+++ javascript.js
@@ -2,35 +2,56 @@
 
 function getComputerChoice(){
     let rndInt = Math.floor(Math.random()*3+1)
-    if(rndInt==1){return "rock";}
-    else if(rndInt==2){return "paper";}
+    if(rndInt===1){return "rock";}
+    else if(rndInt===2){return "paper";}
     else{return "scissors";}
 }
 
 function playRound(playerSelection, computerSelection){
-if(computerSelection=="rock"){
+    computerSelection = getComputerChoice();
+    playerSelection = prompt("Rock, Paper, or Scissors?");
+    playerSelection = playerSelection.toLowerCase();
+    let roundResult;
+if(computerSelection==="rock"){
+    if(playerSelection==="rock"){
+        roundResult="tie"
+    }else if(playerSelection==="paper"){
+        roundResult="win"
+    }else{roundResult="lose"}
+    } else if (computerSelection==="paper"){
     if(playerSelection=="rock"){
-        return "tie"
-    }else if(playerSelection=="paper"){
-        return "lose"
-    }else{return "win"}
-    } else if (computerSelection=="paper"){
-    if(playerSelection=="rock"){
-        return "win"
-    }else if(playerSelection=="paper"){
-        return "tie"
-    }else{return "lose"}
+        roundResult="lose"
+    }else if(playerSelection==="paper"){
+        roundResult="tie"
+    }else{roundResult="win"}
     } else {
-    if(playerSelection=="rock"){
-        return "lose"
-    }else if(playerSelection=="paper"){
-        return "win"
-    }else{return "tie"}
+    if(playerSelection==="rock"){
+        roundResult="win"
+    }else if(playerSelection==="paper"){
+        roundResult="lose"
+    }else{roundResult="tie"}
 }
+let printOut = "You " + roundResult + "! " + playerSelection + " vs " + computerSelection + "."
+console.log(printOut);
+return roundResult;
 }
 
-const computerSelection = getComputerChoice();
-const playerSelection = "rock";
+function game() {
+    let win = 0;
+    let lose = 0;
+    let tie = 0;
+    for(let i=0; i<5; i++){
+        let result = playRound(playerSelection, computerSelection);
+        switch (result){
+            case "win": win++; break;
+            case "lose": lose++; break;
+            case "tie": tie++; break;
+        }
+    }
+    let scoreboard = "Wins: " + win + "   Losses: " + lose + "   Ties: " + tie;
+    console.log(scoreboard);
+}
 
-getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+let computerSelection;
+let playerSelection;
+game();
